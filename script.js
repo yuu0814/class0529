8e7aa2cd6e69dc18233a80f93c4276f078440ef6
yuu0814 <kelly1231997@gmail.com>
2022-05-29 22:40:07 +0800

    add new website

---
+++ script.js
@@ -0,0 +1,14 @@
+
+let listBtn = document.getElementById("list-btn");
+let closeBtn = document.getElementById("close-btn");
+let mobileMenu = document.getElementById("menu-mobile");
+
+
+listBtn.onclick = () => {
+  mobileMenu.classList.toggle("active");
+}    
+
+
+closeBtn.onclick = () => {
+  mobileMenu.classList.toggle("active");
+}
